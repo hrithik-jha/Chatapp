de2e6719b14583edf35bb65e831e075d513ee746
Hrithik Jha <hrithik.jha2017@vitstudent.ac.in>
2018-10-01 22:25:21 +0530

    Initialize MongoDB

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var socket = require('socket.io');
 
-/*var MongoClient = require('mongodb').MongoClient;
+var MongoClient = require('mongodb').MongoClient;
 var url = 'mongodb://localhost/databass';
 MongoClient.connect(url, function(err, db){
     if(err){
@@ -11,7 +11,7 @@ MongoClient.connect(url, function(err, db){
     console.log("Databass initiated.");
     //db.close();
 });
-*/
+
 //Setting up the app
 var app = express();
 var server = app.listen(4200, function(){
